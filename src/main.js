d142502a0e7b6fa359ef49eadb39c441c27e604c
nehemmimosqueda520 <nehemimosqueda8@gmail.com>
2023-10-17 18:14:44 -0300

    debug false

--- src/main.js
+++ src/main.js
@@ -29,7 +29,7 @@ const config = {
     default: "arcade",
     arcade: {
       gravity: { y: 200 },
-      debug: true,
+      debug: false,
     },
   },
   scene: [Preload, Inicio, Menu, Creditos, Ajustes, Pausa, Juego, UI, Animaciones],
